465fbbb80cbd3828c83ec7973755804cd37a97a4
AND2797 <aditya.das2797@gmail.com>
2025-11-25 21:56:21 +0000

    Store item data in .json file. Modify loader to automatically filter while typing and autodisplay information wrt. item. Use with stacked widget

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6 import QtCore
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStyle, QWidget, QHBoxLayout, QLabel, QToolButton, QLineEdit, \
-    QVBoxLayout, QListWidget, QListWidgetItem, QPushButton
+    QVBoxLayout, QListWidget, QListWidgetItem, QPushButton, QStackedWidget
 from PyQt6.QtCore import Qt, QSize, QPropertyAnimation, QRect, QEasingCurve
 from utils.loader import load_items
  # TODO:
@@ -33,6 +33,7 @@ class Window(QMainWindow):
     # - Need to minimise / cross out
     def __init__(self):
         super().__init__()
+        self.cachedData = load_items()
         self.setWindowFlags(
             Qt.WindowType.WindowStaysOnTopHint |
             Qt.WindowType.FramelessWindowHint |
@@ -48,45 +49,94 @@ class Window(QMainWindow):
         # WA_TranslucentBackground makes it completely transparent?
         # self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setWindowOpacity(0.6)
-
+        # Stacked Widget
+        # self.stacked_widget = QStackedWidget(self)
         #minimize
         self.topbar = TopBar(self)
+
+        # stacked
+        self.view_stack = QStackedWidget(self)
 
         #central widget
         self.central_widget = QWidget(self)
         self.layout = QVBoxLayout(self.central_widget)
         self.layout.addWidget(self.topbar)
-        self.setCentralWidget(self.central_widget)
         self.expanded_height = 1000
         self.minimized_height = self.topbar.height() - 10
 
+        # Search
         self.search = QLineEdit(self)
         self.search.setPlaceholderText("Search")
         self.layout.addWidget(self.search)
 
         # search bar
-        recyclables = load_items()
+        items = self.cachedData
         # print(recyclables)
         font = QFont()
         font.setPointSize(16)
         self.search.setFont(font)
         self.list_widget = QListWidget(self)
         self.list_widget.setFont(font)
-        for item in recyclables:
+        for item in items['Name'].tolist():
+            # QListWidgetItem(items.loc[items["Name"] == item], self.list_widget)
             QListWidgetItem(item, self.list_widget)
-        self.layout.addWidget(self.list_widget)
 
-        self.search.textChanged.connect(self.filter_list)
+        self.detail_label = QLabel(self)
+        self.detail_label.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+        self.detail_label.setWordWrap(True)
+
+        self.view_stack.addWidget(self.list_widget)
+        self.view_stack.addWidget(self.detail_label)
+        # self.layout.addWidget(self.list_widget)
+        # self.layout.addWidget(self.detail_label)
+        self.layout.addWidget(self.view_stack)
+        self.setCentralWidget(self.central_widget)
+
+        self.search.textChanged.connect(self.filter_list_and_check)
         # self.list_widget.addItems(recyclables)
         # self.list_widget.setGeometry(10, 10, 480, 980)
         self.is_minimized = False
         self.anim = None
 
-    def filter_list(self, text: str):
-        text = text.strip().lower()
+    def filter_list_and_check(self, text: str):
+        """Filters the list, updates the display and triggers the detail view"""
+        query = text.strip().lower()
+        visible_count = 0
+        single_match_item = None
+
+        # text = text.strip().lower()
         for i in range(self.list_widget.count()):
             item = self.list_widget.item(i)
-            item.setHidden(text not in item.text().lower())
+            item_name = item.text().lower()
+
+            is_match = query in item_name
+
+            item.setHidden(not is_match)
+
+            if is_match:
+                visible_count += 1
+                single_match_item = item
+            print(visible_count)
+
+        if visible_count == 1:
+            self.show_item_details(single_match_item, automatic=True)
+            self.list_widget.setEnabled(False)
+            self.view_stack.setCurrentIndex(1)
+        else:
+            self.detail_label.setText("")
+            self.view_stack.setCurrentIndex(0)
+            self.list_widget.clearSelection()
+            self.list_widget.setEnabled(True)
+
+    def show_item_details(self, item: QListWidgetItem, automatic: bool = False):
+        name = item.text()
+        item_row = self.cachedData[self.cachedData["Name"] == name]
+        # This function can get called repeatedly as long as the user keeps typing.
+        # Reset text everytime to prevent appending
+        self.detail_label.setText("")
+        for col in item_row:
+            new_line = f"\n{col}: {item_row[col].item()}"
+            self.detail_label.setText(self.detail_label.text() + f"{new_line}")
 
     def toggle_minimize(self):
         geo = self.geometry()
--- utils/loader.py
+++ utils/loader.py
@@ -1,8 +1,25 @@
 import os
-def load_items(path=os.path.join(os.getcwd(), 'utils', 'recycle_items.txt')):
+import json
+import pandas as pd
+
+def load_items_obsolete(path=os.path.join(os.getcwd(), 'utils', 'recycle_items.txt')):
     if not os.path.exists(path):
         return []
 
     with open(path, "r", encoding="utf-8") as f:
         lines = [line.strip() for line in f.readlines() if line.strip()]
     return lines
+
+def load_items():
+    base_dir = os.path.dirname(__file__)
+    path = os.path.join(base_dir, "data.json")
+    if not os.path.exists(path):
+        return []
+
+    df = pd.read_json(path)
+    df = pd.json_normalize(df['ArcRaiderItems'])
+    return df
+
+if __name__ == "__main__":
+    df = load_items()
+    print(df)
